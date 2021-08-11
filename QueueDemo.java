20dc93c0456e20f759ea940f1f90da86b95140d6
moshtab <mdmohsin41439@gmail.com>
2021-08-11 20:55:07 +0530

    added a message

---
+++ QueueDemo.java
@@ -0,0 +1,24 @@
+package day14_Queue;
+
+public class QueueDemo {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Queue Computation Problem");
+		System.out.println("The elements of Queue are ");
+
+		Queue<String> queue = new Queue<>(3);
+		queue.push("70");
+		queue.push("30");
+		queue.push("56");
+		
+		System.out.println();
+		System.out.println(queue.peek());
+		System.out.println(queue.poll());
+		System.out.println(queue.poll());
+		
+
+		
+
+	}
+
+}
